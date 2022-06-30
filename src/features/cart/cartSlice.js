32f77af9471b937afb18f844a0eb95a6bd76d4ac
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-30 12:25:15 +0300

    reducer with remove

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,4 +1,6 @@
 //Bir slice nesnesi, Redux storenunun bir parçasını temsil eder(key & value) gibi. Slice yapısı ile Action'lar otomatik olarak oluşturulur.
+//Reducer: State ve action’ı parametre alan ve yeni state’i dönen pure bir fonksiyondur. Action’ın type’ına göre state’i değiştirir ve yeni state’i geri döner.
+//payload : göndereceğimiz veriyi içerir.
 
 import { createSlice } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
@@ -17,9 +19,13 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
